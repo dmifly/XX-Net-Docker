920fbe1da8d64280fb1e5e5dfa9d10ed6b77cc26
dmifly <dmifly.chuang@gmail.com>
2022-03-26 23:44:02 +0800

    Update

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ sh ./release.sh
 # How to Run Docker via Docker Hub Public Image
 docker run --restart=always -p 8085:8085 -p 8087:8087 -p 1080:1080 -d simoncheung/xxnet
 
+
+
 Source:
 https://github.com/XX-net/XX-Net
 
 Docker Hub:
 https://hub.docker.com/r/simoncheung/xxnet
+
+Github Container Registry:
+https://github.com/dmifly/XX-Net-Docker/pkgs/container/xx-net
--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
 set -ex
 # SET THE FOLLOWING VARIABLES
 # docker hub username
-USERNAME=simoncheung
+USERNAME_dh=simoncheung
+# docker hub username
+USERNAME_gh=simoncheung
 # image name
 IMAGE=xxnet
 # ensure we're up to date
@@ -18,7 +20,11 @@ git commit -m "version $version"
 git tag -a "$version" -m "version $version"
 git push
 git push --tags
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+docker tag $USERNAME_dh/$IMAGE:latest $USERNAME_dh/$IMAGE:$version
+docker tag $USERNAME_dh/$IMAGE:latest ghcr.io/$USERNAME_gh/$IMAGE:$version
+docker tag $USERNAME_dh/$IMAGE:latest ghcr.io/$USERNAME_gh/$IMAGE:latest
 # push it
-docker push $USERNAME/$IMAGE:latest
-docker push $USERNAME/$IMAGE:$version
+docker push $USERNAME_dh/$IMAGE:latest
+docker push $USERNAME_dh/$IMAGE:$version
+docker push ghcr.io/$USERNAME_gh/$IMAGE:latest
+docker push ghcr.io/$USERNAME_gh/$IMAGE:$version
