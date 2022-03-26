3df9cac05163090607428a936c5b39a556032f9e
dmifly <dmifly.chuang@gmail.com>
2022-03-26 23:00:18 +0800

    version 4.7.9

--- Backup/version.sh
+++ Backup/version.sh
@@ -1,5 +1,5 @@
 #Get tag from Github
-docker run --rm alpine wget -qO- -t1 -T2 "https://api.github.com/repos/XX-net/XX-Net/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' > GIT_VERSION
+#docker run --rm alpine wget -qO- -t1 -T2 "https://api.github.com/repos/XX-net/XX-Net/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' > GIT_VERSION
 #Read tag from Dockerfile
 grep ENV\ _XXNET Dockerfile | awk -F "=" '{print $2}' > Docker_Version
 #replace process
