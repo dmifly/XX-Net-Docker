dea74c13622a9343a0e303921187d3fd141ed86f
dmifly <dmifly.chuang@gmail.com>
2022-03-26 23:09:39 +0800

    version 4.7.9

--- Backup/version.sh
+++ Backup/version.sh
@@ -1,10 +1,12 @@
 #Get tag from Github
-#docker run --rm alpine wget -qO- -t1 -T2 "https://api.github.com/repos/XX-net/XX-Net/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' > GIT_VERSION
+docker run --rm alpine wget -qO- -t1 -T2 "https://api.github.com/repos/XX-net/XX-Net/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' > GIT_VERSION
 #Read tag from Dockerfile
-grep ENV\ _XXNET Dockerfile | awk -F "=" '{print $2}' > Docker_Version
-#replace process
-#$content1=`cat Docker_Version`; $content2=`cat GIT_Version`; sed "s/${content1}/${content2}/ Dockerfile"
-version1=`cat ../Docker_Version`
-version2=`cat ../GIT_Version`
+grep ENV\ _XXNET ../Dockerfile | awk -F "=" '{print $2}' > Docker_Version
+#get value from existing Dockerfile
+version1=`cat Docker_Version`
+#get value from Github
+version2=`cat GIT_Version`
+#Replace process
 sed -i "" "s/$version1/$version2/" ../Dockerfile
-version2 > ../VERSION
+#Update lastest value to Dockerfile
+grep ENV\ _XXNET ../Dockerfile | awk -F "=" '{print $2}' > ../VERSION
